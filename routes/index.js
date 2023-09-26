e32850f297732ca62a34d667faeeff2c05248703
dengjiada <dengjiada@yunzhangfang.com>
2023-09-26 16:31:29 +0800

    update: 新增登录验证和token验证

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 const pool = require('../mysql/pool')
+const sha256 = require('crypto-js/sha256')
+const jwt = require('jsonwebtoken')
 const fs = require('fs')
 var express = require('express');
 var router = express.Router();
@@ -14,9 +16,30 @@ router.get('/', function (req, res, next) {
   //   })
   // })
   pool.query('SELECT * FROM articledetail', (err, data) => {
-    const {articleId,articleContent} = data[0]
-    res.json({ articleId,articleContent });
+    const { articleId, articleContent } = data[0]
+    res.json({ articleId, articleContent });
   })
 });
+//登录验证
+router.post('/login', (req, res, next) => {
+  const username = 'eb72dccb4540dca6c99ea0beff354aa7c5f9edf5978bb686866c18ef7cfc5957'
+  const password = '6f11e544a4fc4078746c253517a399f0080e6d1db4015af8e9c26d92c4bd032c'
+  if (username === req.body.username && password === req.body.password) {
+    const token = jwt.sign({
+      exp: Math.floor(Date.now() / 1000) + (30 * 60),
+      data: username
+    }, '123456')
+    res.json({ code: 200, token: token })
+  } else {
+    res.json({ code: 401, meg: "用户名或密码错误" })
+  }
 
+})
+//token验证
+router.get('/userInfo', (req, res, next) => {
+  jwt.verify(req.headers.token,'123456',(err)=>{
+    if(err) return res.json({code: 401,msg:'token失效'})
+    return res.json({code: 200,msg:'token有效'})
+  })
+})
 module.exports = router;
