83d822a2c53c43210279301a8cc41a338ce297fa
dengjiada <dengjiada@yunzhangfang.com>
2023-10-13 14:50:00 +0800

    update: update

--- mysql/pool.js
+++ mysql/pool.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: '0.0.0.0',
+    host: 'localhost',
     port: '3306',
     user: 'root',
-    password: '你的密码',
+    password: 'woaini121',
     database: 'myblog'
 });
 module.exports = pool 
