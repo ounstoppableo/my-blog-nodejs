bd0224efc69edcb0c5975946a3e3f8a0b9d6e2ce
unstoppable840 <unstoppable840@gmail.com>
2023-10-14 12:08:02 +0800

    update: 进行公私分离

---
+++ mysql/pool - public.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql')
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'woaini121',
+    database: 'myblog'
+});
+module.exports = pool 
